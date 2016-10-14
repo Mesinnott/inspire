e25356f3deadc524eb0e041d7298ee5a62ea8305
Michael Sinnott <mesinnott@gmail.com>
2016-10-14 15:00:33 -0600

    to do list is working

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -5,14 +5,79 @@
 
 	todoService.getTodos
 
-
-
 	todoService.saveTodos
 	// new up the TodoService that has already been configured for your use
-	// There are two methods getTodos returns and array
+	// There are two methods getTodos returns an array
 	// saveTodos accepts an array and stores it to your local storage
 	
+
+
+
+
+
+
+	var printTodos = function(){
+		var tasks= todoService.getTodos()
+		var taskTemplate= `Total Tasks on the docket: ${tasks.length}`
+		for(var i=0; i<tasks.length; i++){
+			var thisTask=tasks[i]
+			taskTemplate += `<li><i id='${i}' class="fa fa-circle-o ${i}" aria-hidden="true"></i> ${i+1}: ${thisTask}<i id='${i}'class="fa fa-times" aria-hidden="true"></i></li>`
+		}
+		$('#taskList').html(taskTemplate)
+		// $('.fa-check-circle').hide()
+	}
+
+	printTodos()
+
+
+	var addTodo = function(todo){
+		var tasks= todoService.getTodos()
+		tasks.push(todo)
+		todoService.saveTodos(tasks)
+	}
+
+	var deleteTodo = function(id){
+		var tasks=todoService.getTodos()
+		var newTasks =tasks.splice(id,1)
+		todoService.saveTodos(tasks)
+	}
+
+	$('#addToDo').on('click', function(e){
+		e.preventDefault();
+		var task = $('#addField').val()
+		var var1 = document.getElementById('myForm');
+		addTodo(task)
+		printTodos()
+		var1.reset()
 	
+	})
 	
+	$('#taskList').on('click','.fa-times', function(e){
+		deleteTodo(this.id)
+		printTodos()
+	})
+
+	$('#taskList').on('click','.fa-circle-o', function(e){
+		e.preventDefault();
+		console.log(this.id)
+		console.log('cLICKy')
+		var yep= this.id
+
+		$(this).toggleClass('fa-circle-o  fa-check-circle ')
+
+		// $('.fa-circle-o').hide()
+		// $('.fa-check-circle ').show()
+		
+	})
+	
+	$('#taskList').on('click','.fa-check-circle', function(e){
+		e.preventDefault();
+		console.log(this.id)
+		console.log('cLICKy')
+		var yep= this.id
+
+		$(this).toggleClass('fa-check-circle fa-circle-o')
+		})
+
 	
 }())
--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -1,21 +1,21 @@
-(function (){
-	
+(function () {
+
 	var wc = this;
 	var weatherService = new WeatherService();
-	
-	weatherService.getWeather(function(weather){
-		weather=JSON.parse(weather)
-	
-		var main= weather.weather[0].main
-		var description=weather.weather[0].description
+
+	weatherService.getWeather(function (weather) {
+		weather = JSON.parse(weather)
+
+		var main = weather.weather[0].main
+		var description = weather.weather[0].description
 		var icon = weather.weather[0].icon
 		var temp = weather.main.temp
 		var city = weather.name
 
-		var tempF = temp*9/5-459.67
-		var tempC = temp-273.15
-		tempC=Math.floor(tempC)
-		tempF=Math.floor(tempF)
+		var tempF = temp * 9 / 5 - 459.67
+		var tempC = temp - 273.15
+		tempC = Math.floor(tempC)
+		tempF = Math.floor(tempF)
 
 		var iconUrl = `http://openweathermap.org/img/w/${icon}.png`
 
@@ -24,26 +24,28 @@
 							<button class='btn' id='c'>${tempC} C</button>
 							${main} </h4>
 						<h5><image src='${iconUrl}' alt=''></h5>
-						<h5> Specifically: ${description}</h5>` 
+						<h5> Specifically: ${description}</h5>`
 
 		$('#weather').append(template)
+
 		$('#c').hide()
-		$('#f').on('click', function(e){
-				e.preventDefault()
-				$('#f').hide()
-				$('#c').show()
+
+		$('#f').on('click', function (e) {
+			e.preventDefault()
+			$('#f').hide()
+			$('#c').show()
 		})
-		$('#c').on('click', function(e){
-				e.preventDefault()
-				$('#c').hide()
-				$('#f').show()
+		$('#c').on('click', function (e) {
+			e.preventDefault()
+			$('#c').hide()
+			$('#f').show()
 		})
 
 		//What can you do with this weather object?
 	})
-	
-	
-	
-	
-	
-}())
+
+
+
+
+
+} ())
