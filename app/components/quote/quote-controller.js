8556c8a8bab0d79118f03ce911a2eba4da3e46db
Michael Sinnott <mesinnott@gmail.com>
2016-10-14 12:35:58 -0600

    toggle from c to f

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -7,11 +7,26 @@
 		quote= JSON.parse(quote)
 		var quoteText= quote.quote
 		var author = quote.author
-		var quoteTemplate=`<h2>Ponder this today: "${quoteText}"</h2>
-								<h5>--${author}</h5>`
-		$('#quote').append(quoteTemplate)
+		var quoteTemplate=`<h2>Ponder this today: "${quoteText}"</h2>`
+		var authorTemplate=`<h5>--${author}</h5>`
+		$('#quote').html(quoteTemplate)
+
+		$('#quote').on('mouseenter', function (e){
+			console.log('enter')
+			e.preventDefault()
+
+			$('#quote').html(authorTemplate)
+		})
+		$('#quote').on('mouseleave', function (e){
+			console.log('clicksy')
+			e.preventDefault()
+
+			$('#quote').html(quoteTemplate)
+		})
+
+		})
 
 		
-	})
+	
 	
 }())
--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -4,31 +4,40 @@
 	var weatherService = new WeatherService();
 	
 	weatherService.getWeather(function(weather){
-		// console.log(weather);
 		weather=JSON.parse(weather)
 	
 		var main= weather.weather[0].main
-		// console.log(main)
 		var description=weather.weather[0].description
-		// console.log(description)
 		var icon = weather.weather[0].icon
 		var temp = weather.main.temp
-		// console.log(temp)
 		var city = weather.name
-		// console.log(city)
 
 		var tempF = temp*9/5-459.67
+		var tempC = temp-273.15
+		tempC=Math.floor(tempC)
 		tempF=Math.floor(tempF)
 
 		var iconUrl = `http://openweathermap.org/img/w/${icon}.png`
 
 		var template = `<h3>Today's weather in ${city}:<h3>
-						<h4> ${tempF}F  ${main} </h4>
+						<h4> <button class='btn' id='f'>${tempF} F</button>
+							<button class='btn' id='c'>${tempC} C</button>
+							${main} </h4>
 						<h5><image src='${iconUrl}' alt=''></h5>
 						<h5> Specifically: ${description}</h5>` 
 
 		$('#weather').append(template)
-
+		$('#c').hide()
+		$('#f').on('click', function(e){
+				e.preventDefault()
+				$('#f').hide()
+				$('#c').show()
+		})
+		$('#c').on('click', function(e){
+				e.preventDefault()
+				$('#c').hide()
+				$('#f').show()
+		})
 
 		//What can you do with this weather object?
 	})
