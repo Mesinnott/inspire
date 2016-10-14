267de070ad9f482c4c227830db5dce0a6cedd0b6
Michael Sinnott <mesinnott@gmail.com>
2016-10-14 16:54:17 -0600

    end of day

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -12,10 +12,10 @@
 	
 	var printTodos = function(){
 		var tasks= todoService.getTodos()
-		var taskTemplate= `Total Tasks on the docket: ${tasks.length}`
+		var taskTemplate= `Total Tasks on the Docket: ${tasks.length}<hr>`
 		for(var i=0; i<tasks.length; i++){
 			var thisTask=tasks[i]
-			taskTemplate += `<li><i id='${i}' class="fa fa-circle-o ${i}" aria-hidden="true"></i> ${i+1}: ${thisTask}<i id='${i}'class="fa fa-times" aria-hidden="true"></i></li>`
+			taskTemplate += `<li id='${i}'><i id='${i}' class="fa fa-circle-o" aria-hidden="true"></i> ${i+1}: ${thisTask}<i id='${i}'class="fa fa-times" aria-hidden="true"></i></li>`
 		}
 		$('#taskList').html(taskTemplate)
 		// $('.fa-check-circle').hide()
@@ -45,6 +45,18 @@
 		var1.reset()
 	
 	})
+
+	$("input").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        var task = $('#addField').val()
+		var var1 = document.getElementById('myForm');
+		addTodo(task)
+		printTodos()
+		var1.reset()
+    }
+});
+
 	
 	$('#taskList').on('click','.fa-times', function(e){
 		deleteTodo(this.id)
@@ -53,23 +65,16 @@
 
 	$('#taskList').on('click','.fa-circle-o', function(e){
 		e.preventDefault();
-		console.log(this.id)
-		console.log('cLICKy')
-		var yep= this.id
-
 		$(this).toggleClass('fa-circle-o  fa-check-circle ')
+		var yep= this.id
+		console.log(this.id)
 
-		// $('.fa-circle-o').hide()
-		// $('.fa-check-circle ').show()
-		
+		$('#yep').addClass('strikethrough')
 	})
 	
 	$('#taskList').on('click','.fa-check-circle', function(e){
 		e.preventDefault();
 		console.log(this.id)
-		console.log('cLICKy')
-		var yep= this.id
-
 		$(this).toggleClass('fa-check-circle fa-circle-o')
 		})
 
